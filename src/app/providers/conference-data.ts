da339bd9bb47eae52ac5bcc1f23cfc896f64fc88
Kaiam Rodrigues <kaiam.rodrigues@totvs.com.br>
2019-06-07 18:09:51 -0300

    adjusting filter

--- src/app/providers/conference-data.ts
+++ src/app/providers/conference-data.ts
@@ -23,28 +23,49 @@ export class ConferenceData {
     return this.load().pipe(
       map((data: any) => {
 
-        const dataReturn = [];
+        let dataReturn = [];
         const dataFilter = [];
         let search = null;
+        let order = null;
 
         data.forEach( element => {
 
-          search = element.event_name.includes(queryText);
+          search = dataReturn.map(function(e) { return e.date; }).indexOf(this.formatDate(element.dateTimeStart));
 
-          if (search) {
-            dataFilter.push({date: this.formatDate(element.dateTimeStart), itens: [element]});
-          } else {
+          if ( search === -1 ) {
             dataReturn.push({date: this.formatDate(element.dateTimeStart), itens: [element]});
+          } else {
+            dataReturn[search].itens.push(element);
           }
         });
 
+        if (queryText) {
+          dataReturn.forEach( element => {
+
+            element.itens.forEach( element => {
+
+            search = element.event_name.toLowerCase().includes(queryText.toLowerCase());
+  
+            if (search) {
+              dataFilter.push({date: this.formatDate(element.dateTimeStart), itens: [element]});
+            }
+          });
+
+        });
+        }
+        
         if(dataFilter.length) {
           return dataFilter; 
         } else { 
           return dataReturn;
         }
+
       })
     );
+  }
+
+  order(data) {
+
   }
 
   load(): any {
