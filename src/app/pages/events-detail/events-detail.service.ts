d1bad6a0b7f321e2e8a622c97b5e33c3c6e7dbdf
Matheus Bizutti <matheus.bizutti@totvs.com.br>
2019-06-10 15:07:13 -0300

    set authorization headers

--- src/app/pages/events-detail/events-detail.service.ts
+++ src/app/pages/events-detail/events-detail.service.ts
@@ -4,21 +4,36 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 import { BaseUrlService } from '../../base-url/base-url.service';
+import { LoginComponent } from '../login/login.component';
 
 @Injectable()
 export class EventsDetailService {
 
   private eventsSurvey = this.baseUrl.getBaseUrl() + 'survey/';
   private eventsSubscription = this.baseUrl.getBaseUrl() + 'subscribe/';
+  private user;
 
-  constructor(private http: HttpClient, private baseUrl: BaseUrlService) { }
+  constructor(private http: HttpClient, private baseUrl: BaseUrlService, private auth: LoginComponent) {
+    this.getUser();
+  }
 
   survey(body = {}): Observable<any> {
-    return this.http.post(this.eventsSurvey, body);
+
+    const options = {
+      headers: {
+        'Authorization': `${this.user.token_type} ${this.user.access_token}`
+      }
+    };
+
+    return this.http.post(this.eventsSurvey, body, options);
   }
 
   subscription(body = {}): Observable<any> {
     return this.http.post(this.eventsSubscription, body);
+  }
+
+  async getUser() {
+    return this.user = await this.auth.getUser();
   }
 
 }
