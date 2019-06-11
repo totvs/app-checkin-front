b36e5de7b23873705fd34c5730fb3d665f0f33f6
Matheus Bizutti <matheus.bizutti@totvs.com.br>
2019-06-11 17:59:46 -0300

    set token in subscription api call

--- src/app/pages/events-detail/events-detail.service.ts
+++ src/app/pages/events-detail/events-detail.service.ts
@@ -12,24 +12,23 @@ export class EventsDetailService {
   private eventsSurvey = this.baseUrl.getBaseUrl() + 'v1/survey/';
   private eventsSubscription = this.baseUrl.getBaseUrl() + 'subscription/';
   private user;
+  private options = {};
 
   constructor(private http: HttpClient, private baseUrl: BaseUrlService, private auth: LoginComponent) {
     this.getUser();
-  }
-
-  survey(body = {}): Observable<any> {
-
-    const options = {
+    this.options = {
       headers: {
         'Authorization': `${this.user.token_type} ${this.user.access_token}`
       }
     };
+  }
 
-    return this.http.post(this.eventsSurvey, body, options);
+  survey(body = {}): Observable<any> {
+    return this.http.post(this.eventsSurvey, body, this.options);
   }
 
   subscription(body = {}): Observable<any> {
-    return this.http.post(this.eventsSubscription, body);
+    return this.http.post(this.eventsSubscription, body, this.options);
   }
 
   async getUser() {
