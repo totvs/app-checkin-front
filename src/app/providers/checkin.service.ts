60555ed6a18afff927cac6963600318ce194ca99
Kaiam Rodrigues <kaiam.rodrigues@totvs.com.br>
2019-06-13 14:11:53 -0300

    oidc test

--- src/app/providers/checkin.service.ts
+++ src/app/providers/checkin.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 
 import { BaseUrlService } from '../base-url/base-url.service';
 import { UtilsService } from '../utils.service.ts/utils.service';
+import { LoginComponent } from '../pages/login/login.component';
 
 
 @Injectable({
@@ -17,11 +18,19 @@ export class CheckinService {
   constructor(
     public http: HttpClient,
     private baseUrlService: BaseUrlService,
-    private utilsService: UtilsService
+    private utilsService: UtilsService,
+    private auth: LoginComponent
   ) {}
 
-  signup(body = {}): Observable<any> {
-    return this.http.post(this.baseUrlService.getBaseUrl() + 'v1/signup', body, this.utilsService.setHeader());
+  async signup(body = {}): Promise<Observable<any>> {
+    const options = {headers: {}};
+    const user = await this.auth.getUser();
+
+    options.headers = {
+      'Authorization': `${user.token_type} ${user.access_token}`
+    };
+
+    return this.http.post(this.baseUrlService.getBaseUrl() + 'v1/signup', body, options);
   }
 
 }
